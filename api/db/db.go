432b24c409ff3b433bd86e8bc4b4ea7154efd569
victor <viheanaco@gmail.com>
2019-11-23 08:50:38 +0100

    inititalize connection to gorm/postgres #2

--- api/db/db.go
+++ api/db/db.go
@@ -1,1 +1,35 @@
 package db
+
+import (
+	"log"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	"github.com/victor-nach/nigerian-senators/api/models"
+)
+
+var db *gorm.DB
+
+func init() {
+	dbURL, ok := os.LookupEnv("DATABASE_URL")
+	if !ok {
+		dbURL = "postgres://xrqfqmoo:5jTTWJqZsQoWXCsUl5MVwZ2CO2FXiycB@salt.db.elephantsql.com:5432/xrqfqmoo"
+	}
+	var err error
+	db, err = gorm.Open("postgres", dbURL)
+
+	if err != nil {
+		log.Println("Could not connect to the db")
+		log.Fatal(err)
+	}
+	log.Println("Succesfully connected to the db")
+	defer db.Close()
+
+	db.Debug().AutoMigrate(&models.Senators{})
+}
+
+func Db() *gorm.DB {
+	return db
+}
--- api/models/senators.go
+++ api/models/senators.go
@@ -1,9 +1,22 @@
 package models
 
 import (
-
+	"github.com/victor-nach/nigerian-senators/api/db"
 )
 
 type Senators struct {
-	FirstName string
+	FullName		string
+	State			string
+	District		string
+	PhoneNumber		string
+	email			string
+}
+
+// db := db.Db()
+
+
+// GetAll - returns an array containing all the senators
+func (s *Senators) GetAll() (*[]Senators, error) {
+	senators := []Senators{}
+	err := db.Db().Debug.Find(&Senators).Error
 }
