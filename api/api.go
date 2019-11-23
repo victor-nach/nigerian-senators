14167d4171c0fd8fa57a44996328c94600926ffc
victor <viheanaco@gmail.com>
2019-11-23 08:12:13 +0100

    bg: fix port binfing issue ##2

--- api/api.go
+++ api/api.go
@@ -3,14 +3,17 @@ package api
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/victor-nach/nigerian-senators/api/routes"
 )
 
 
 func Run() {
-	log.Println("welcome")
-	PORT := "8080"
+	PORT, ok := os.LookupEnv("PORT")
+	if !ok {
+		PORT = "8080"
+	}
 	log.Println("serving on port:", PORT)
 
 	log.Fatal(http.ListenAndServe(":"+PORT, routes.Router()))
